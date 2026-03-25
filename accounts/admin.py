ecc820233304c4307aed55e19b44dfa6434f3230
Rinku152004 <rinkumetaliya3@gmail.com>
2026-03-25 11:52:28 +0530

    add Djangoproject

--- accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User, Role, Permission
-
-
-admin.site.register(User)
-admin.site.register(Role)
-admin.site.register(Permission)
--- roles/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-
-    path('', views.roles_list, name="roles_list"),
-    path('create_role/', views.create_role, name="create_role"),
-]
